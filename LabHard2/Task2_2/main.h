6a3945341c463c1ddd298cad0b3ef71c80808804
Daniil Nesterov <daniilnesterov15@mail.ru>
2021-06-14 15:16:15 +0300

    Add files via upload

---
+++ LabHard2/Task2_2/main.h
@@ -0,0 +1,4 @@
+#include "stm32f0xx.h" 				//Подключение библиотеки с моделью stm32f0xx
+void InitUSART1(void); 				//Декларация функции инициализации USART1
+void msg (uint8_t,unsigned);	//Декларация функции передачи данных
+
